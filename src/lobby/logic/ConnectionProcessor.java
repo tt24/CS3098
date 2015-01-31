7304577f391f1ecb37fec8db5e452aba1494afd4
CodeBreaker <>
2015-01-31 13:04:58 +0000

    Addded logging, made basic structure of lobby connection processor

--- src/lobby/logic/ConnectionProcessor.java
+++ src/lobby/logic/ConnectionProcessor.java
@@ -1,5 +1,25 @@
 package lobby.logic;
 
-public class ConnectionProcessor {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class ConnectionProcessor {
+	private final int portNumber = 1777;
+	private String hostname;
+	public ConnectionProcessor(){
+		try {
+			hostname = InetAddress.getLocalHost().getHostName();
+			Socket lobbySocket = new Socket(hostname, portNumber);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void createAGame(){
+		
+	}
 }
---
+++ src/logging/Logger.java
@@ -0,0 +1,28 @@
+package logging;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import javax.swing.JOptionPane;
+
+public class Logger {
+	
+	
+	public static void log(String details){
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("the-file-name.txt", "UTF-8");
+			writer.append("\n"+details);
+			writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void logAndDisplayError(String userMessage, String errorDetails){
+		JOptionPane.showMessageDialog(null, userMessage);
+		log(errorDetails);
+	}
+}
